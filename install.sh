5118a9baa2cb380345bd24f1baf2ed645b474cfb
hippojamba <oliver.sabler@gmail.com>
2018-09-22 13:46:11 +0200

    Fix broken install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOTFILES_PATH
-DOTFILES_PATH="$( cd "$( dirname "${BASH_SOs
+DOTFILES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s "$DOTFILES_PATH/.bash_profile" ~
 
 # RC
